aa1bcecc16540dd7c86413729cd9342ada9d5e7a
MCKENT05 <atemitope95@gmail.com>
2022-05-04 09:40:42 +0100

    Fixed linter errors

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CategoriesController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     all_categories = Category.all
