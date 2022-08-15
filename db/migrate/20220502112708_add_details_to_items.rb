47c1cdbb5205430e2b4049f7a9763b579650fd75
MCKENT05 <atemitope95@gmail.com>
2022-08-15 12:59:05 +0100

    Added default image for items

--- db/migrate/20220502112708_add_details_to_items.rb
+++ db/migrate/20220502112708_add_details_to_items.rb
@@ -2,7 +2,7 @@ class AddDetailsToItems < ActiveRecord::Migration[7.0]
   def change
     add_column :items, :name, :string
     add_column :items, :description, :string
-    add_column :items, :image, :string
+    add_column :items, :image, :string, :default => 'https://res.cloudinary.com/duj88gras/image/upload/v1660563933/default_nnor2h.png'
     add_column :items, :measurement_unit, :string
     add_column :items, :quantity, :integer
   end
