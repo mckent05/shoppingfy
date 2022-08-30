958de6f2a85de092b960dd68d2770be57bc4c3d4
MCKENT05 <atemitope95@gmail.com>
2022-08-30 12:25:49 +0100

    Added validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   has_many :items, class_name: 'Item', foreign_key: 'user_id', dependent: :destroy
   has_many :categories, class_name: 'Category', foreign_key: 'user_id', dependent: :destroy
   has_many :carts, class_name: 'Cart', foreign_key: 'user_id', dependent: :destroy
+  
+
+  validates :username, presence: true, uniqueness: true
 end
